4490e667be779a828f44a645e3ceab6f46f6c3d4
Drew Taylor <dretaylo@cisco.com>
2018-11-17 15:21:17 -0800

    beginning chrome extension

--- background.js
+++ background.js
@@ -1,11 +1,14 @@
-// this is the background code...
-
-console.log("hi");
-
-// listen for our browerAction to be clicked
-chrome.browserAction.onClicked.addListener(function (tab) {
-	// for the current tab, inject the "inject.js" file & execute it
-	chrome.tabs.executeScript(tab.ib, {
-		file: 'inject.js'
+chrome.runtime.onInstalled.addListener(function() {
+	chrome.storage.sync.set({color: '#3aa757'}, function() {
+		console.log("The color is green.");
+	});
+	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+		chrome.declarativeContent.onPageChanged.addRules([{
+			conditions: [new chrome.declarativeContent.PageStateMatcher({
+				pageUrl: {hostEquals: 'developer.chrome.com'},
+			})
+			],
+				actions: [new chrome.declarativeContent.ShowPageAction()]
+		}]);
 	});
 });
